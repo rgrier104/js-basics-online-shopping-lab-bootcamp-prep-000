7c88cfe2bb6e9c82df59a7208702ce513aecb12b
Rachel Grier <rgrier104@gmail.com>
2018-08-04 20:36:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i <cart.length; i++){
+  for (var i = 0; i < cart.length; i++){
     if (cart.includes(item)) {
     cart.splice(i+1,1)
   }  else {
