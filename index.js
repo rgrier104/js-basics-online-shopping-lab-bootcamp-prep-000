2e558a8cdddd9b0559a4349a0018366c49a86966
Rachel Grier <rgrier104@gmail.com>
2018-08-04 21:15:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,11 +44,9 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if (cart[i].hasOwnProperty(item)) {
     cart.splice(i,1)
-    return cart
   } else {
     console.log(`That item is not in your cart.`)
-    return cart
-  }
+  } return cart
 }
 }
 
