4483c43b3aa364bf8cc2e0351640b35c97e9a64e
Rachel Grier <rgrier104@gmail.com>
2018-08-04 23:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ function placeOrder(cardNumber) {
     return `Sorry, we don\'t have a credit card on file for you.`
   } else {
     cart = []
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }
 }
