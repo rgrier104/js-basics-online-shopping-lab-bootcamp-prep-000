4a4b55c7eb7986469ecbf73d2f545f86fd94fd75
Rachel Grier <rgrier104@gmail.com>
2018-08-04 20:03:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function viewCart() {
   }
   else {
     var output = `In your cart, you have `
-    for(var i = 0; i < cart.length - 1; i++) {
-      output += `${cart[i].itemName} at ${cart[i].itemPrice}, `
+    for(var i = 1; i < cart.length - 1; i++) {
+      output += `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
   } return output
 }
