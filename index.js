ee5f7675ae6251835067077636bb190046e309e3
Rachel Grier <rgrier104@gmail.com>
2018-08-04 19:37:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ function viewCart() {
   if (cart.length === 0){
     return `Your shopping cart is empty.`
   } else if (cart.length === 1) {
-    return `In your cart, you have ${cart[i].itemName} at ${cart[i].itemPrice}.`
+    return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
   }
   else {
     for(var i = 0; i < cart.length - 1; i++) {
       var output = [`In your cart, you have `]
-      output.push(`${cart[i].itemName} at ${cart[i].itemPrice},`)
+      output = output + `${cart[i].itemName} at ${cart[i].itemPrice},`
     }
   } return output
 }
