19babe540d1b39eba05586bfd765c2bb6db37439
Rachel Grier <rgrier104@gmail.com>
2018-08-04 22:39:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,13 +46,13 @@ function removeFromCart(item) {
     cart.splice(i,1)
     return cart
   }
-} return `That item is not in your cart.` 
+} return `That item is not in your cart.`
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === undefined) {
+  if (cardNumber === null) {
     console.log(`Sorry, we don't have a card on file for you.`)
   } else {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardnumber}`)
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
 }
