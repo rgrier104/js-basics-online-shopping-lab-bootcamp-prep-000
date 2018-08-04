ed03b1e2d99920ad6e424a3b7b24853b4843aa29
Rachel Grier <rgrier104@gmail.com>
2018-08-04 23:01:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,6 +55,5 @@ function placeOrder(cardNumber) {
   } else {
     return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
-  cart = []
-  return cart
+  cart.length = 0
 }
